cfdc1567e31f8935b1268fd969e16c08d2f23795
Nathcaa <nathan.cancel@epitech.eu>
2024-05-18 21:33:59 +0200

    [FIX] remove rectangle and start to replace it by a character

--- start_game.py
+++ start_game.py
@@ -20,7 +20,7 @@ class Player:
     def __init__(self, screen_width, screen_height):
         self.x = screen_width // 2
         self.y = screen_height // 2
-        self.speed = 5
+        self.speed = 50
         self.size = 50
 
     def move_player(self, keys):
@@ -32,6 +32,8 @@ class Player:
             self.y -= self.speed
         elif keys[pygame.K_DOWN]:
             self.y += self.speed
+        player.x = max(0, min(self.x, WIDTH - self.size))
+        player.y = max(0, min(self.y, HEIGHT - self.size))
 
 clock = pygame.time.Clock()
 
@@ -42,23 +44,19 @@ print("%d\n", len(bg))
 nb = 0
 running = True
 while running:
-    screen.fill(WHITE)
+    #screen.fill(WHITE)
 
-    screen.blit(bg[int(nb / 9 % 15)], (0,0))
-    nb += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
-    pygame.display.flip()
 
     keys = pygame.key.get_pressed()
     player.move_player(keys)
+    screen.blit(bg[int(nb / 9 % 15)], (0,0))
+    nb += 1
 
-    player.x = max(0, min(player.x, WIDTH - player.size))
-    player.y = max(0, min(player.y, HEIGHT - player.size))
+    pygame.display.flip()
 
-    pygame.draw.rect(screen, RED, player_rect)
 
     pygame.display.flip()
     clock.tick(60)
