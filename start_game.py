4f84e78f77b31998e9850d710d0538697ede3395
Nathcaa <nathan.cancel@epitech.eu>
2024-05-18 17:09:30 +0200

    [ADD] create folder with volcano pictures

--- start_game.py
+++ start_game.py
@@ -1,45 +1,62 @@
 import pygame
+import pygame.locals 
 import sys
-
-pygame.init()
-
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Déplacement du joueur")
+import os
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 
-player_size = 50
-player_rect = pygame.Rect((screen_width - player_size) // 2, (screen_height - player_size) // 2, player_size, player_size)
-player_speed = 5
+WIDTH = 1920
+HEIGHT = 1080
+
+pygame.init()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Déplacement du joueur")
+
+bg = [pygame.transform.scale(pygame.image.load(os.path.join('assets', f"volcano{str(i)}.png")).convert(), (WIDTH, HEIGHT - 69)) for i in range(1, 17)]
+
+class Player:
+    def __init__(self, screen_width, screen_height):
+        self.x = screen_width // 2
+        self.y = screen_height // 2
+        self.speed = 5
+        self.size = 50
+
+    def move_player(self, keys):
+        if keys[pygame.K_LEFT]:
+            self.x -= self.speed
+        elif keys[pygame.K_RIGHT]:
+            self.x += self.speed
+        elif keys[pygame.K_UP]:
+            self.y -= self.speed
+        elif keys[pygame.K_DOWN]:
+            self.y += self.speed
 
 clock = pygame.time.Clock()
 
-def move_player(keys):
-    if keys[pygame.K_LEFT]:
-        player_rect.x -= player_speed
-    elif keys[pygame.K_RIGHT]:
-        player_rect.x += player_speed
-    elif keys[pygame.K_UP]:
-        player_rect.y -= player_speed
-    elif keys[pygame.K_DOWN]:
-        player_rect.y += player_speed
+player = Player(WIDTH, HEIGHT)
+player_rect = pygame.Rect((WIDTH - player.size) // 2, (HEIGHT - player.size) // 2, player.size, player.size)
 
+print("%d\n", len(bg))
+nb = 0
 running = True
 while running:
     screen.fill(WHITE)
 
+    screen.blit(bg[int(nb / 9 % 15)], (0,0))
+    nb += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
+    pygame.display.flip()
 
     keys = pygame.key.get_pressed()
-    move_player(keys)
+    player.move_player(keys)
 
-    player_rect.x = max(0, min(player_rect.x, screen_width - player_size))
-    player_rect.y = max(0, min(player_rect.y, screen_height - player_size))
+    player.x = max(0, min(player.x, WIDTH - player.size))
+    player.y = max(0, min(player.y, HEIGHT - player.size))
 
     pygame.draw.rect(screen, RED, player_rect)
 
