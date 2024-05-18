b0425639a40e6d772e43c9fbcd00447f71874de3
t._iphaine <tiphaine.pautonnier@epitech.eu>
2024-05-18 23:12:05 +0200

    fix: neutral rest position added for jean-mich.

--- player.py
+++ player.py
@@ -6,14 +6,15 @@ class Player:
     def __init__(self, screen_width, screen_height):
         self.images_left = [pygame.image.load(os.path.join('assets/jean-mich', f"jean-mich{i}.png")) for i in range(1, 5)]
         self.images_right = [pygame.image.load(os.path.join('assets/jean-mich', f"jean-mich{i}.png")) for i in range(5, 9)]
-        self.image = self.images_right[0]
+        self.image_neutral = pygame.image.load(os.path.join('assets/jean-mich', "jean-mich9.png"))
+        self.image = self.image_neutral
         self.size = self.image.get_width()
         self.x = (screen_width - self.size) // 2
         self.y = screen_height - self.image.get_height() - 55
         self.speed = 10
         self.animation_index = 0
         self.animation_counter = 0
-        self.direction = 'right'
+        self.direction = 'neutral'
 
     def move_player(self, keys):
         moved = False
@@ -38,5 +39,7 @@ class Player:
                     self.image = self.images_right[self.animation_index]
                 else:
                     self.image = self.images_left[self.animation_index]
+        else:
+            self.image = self.image_neutral
 
 player = Player(WIDTH, HEIGHT)
